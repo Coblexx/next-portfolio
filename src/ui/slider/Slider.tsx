9aa267d150752aa7f3c1630bc9117f2fd57d1184
radek <radek.glogowski3@gmail.com>
2024-05-05 17:26:12 +0200

    feat: start working on project slider

--- src/ui/slider/Slider.tsx
+++ src/ui/slider/Slider.tsx
@@ -1,3 +1,18 @@
+// components
+
+// icons
+import { FiArrowRight } from "react-icons/fi";
+import { FiArrowLeft } from "react-icons/fi";
+
 export default function Slider() {
-  return <div></div>;
+  return (
+    <div>
+      <button>
+        <FiArrowLeft />
+      </button>
+      <button>
+        <FiArrowRight />
+      </button>
+    </div>
+  );
 }
--- src/ui/slider/SliderItem.tsx
+++ src/ui/slider/SliderItem.tsx
@@ -1,3 +1,23 @@
-export default function Slider() {
-  return <div></div>;
+// react
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
+
+export default function SliderItem({
+  title,
+  desc,
+  link = "",
+}: {
+  title: String;
+  desc: String;
+  link?: Url;
+}) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div>
+        <p>{desc}</p>
+        <Link href={link}></Link>
+      </div>
+    </div>
+  );
 }
