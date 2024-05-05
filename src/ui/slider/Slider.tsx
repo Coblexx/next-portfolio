60bd13f23fa3728f738c4672c687d7625e7dc939
radek <radek.glogowski3@gmail.com>
2024-05-05 18:26:19 +0200

    feat:change slide

--- src/ui/slider/Slider.tsx
+++ src/ui/slider/Slider.tsx
@@ -1,17 +1,48 @@
+"use client";
+
 // components
+import SliderItem from "./SliderItem";
 
 // icons
 import { FiArrowRight } from "react-icons/fi";
 import { FiArrowLeft } from "react-icons/fi";
 
+// data
+import slidesData from "@/slides.json";
+import { useState } from "react";
+
 export default function Slider() {
+  const [currSlide, setCurrSlide] = useState(0);
+
+  const slides = Object.entries(slidesData);
+  const [title, { desc, link }] = slides[currSlide];
+
+  function handleSlideChange(prev = false) {
+    if (!prev) {
+      setCurrSlide((id) => (id < slides.length - 1 ? id + 1 : 0));
+    } else {
+      setCurrSlide((id) => (id !== 0 ? id - 1 : slides.length - 1));
+    }
+  }
+
   return (
     <div>
-      <button>
-        <FiArrowLeft />
+      <button
+        onClick={() => {
+          handleSlideChange(true);
+        }}
+      >
+        <FiArrowLeft size={40} />
       </button>
-      <button>
-        <FiArrowRight />
+      <div>
+        <SliderItem title={title} desc={desc} link={link} />
+      </div>
+      <button
+        onClick={() => {
+          handleSlideChange();
+        }}
+      >
+        <FiArrowRight size={40} />
       </button>
     </div>
   );
